a645828897f9dc4c46e69b0d98eeb0433f5357a7
Edd <smith.edward@hotmail.com>
2019-11-17 13:25:39 +0000

    updated every instance of coords in MS to use index instead

--- src/components/Minesweeper.vue
+++ src/components/Minesweeper.vue
@@ -18,7 +18,8 @@
         		minesweeper_config : {
 					width : 12,
 					height: 16,
-					bomb_count : Math.floor( Math.random() * 45 ) + 15,
+//					bomb_count : Math.floor( Math.random() * 45 ) + 15,
+					bomb_count : 40,
 				},
 				bombs_coordinates : [],
 				game_over : false,
@@ -33,60 +34,56 @@
 
         mounted () {
         	this.populate_grid_with_bombs();
+            this.show_all_bombs();
         },
 
         methods: {
         	handle_tile_left_click ( index ) {
-        		const index_coords = { x : index % this.minesweeper_config.width === 0 ? this.minesweeper_config.width : index % this.minesweeper_config.width , y : Math.ceil( index / this.minesweeper_config.width ) }
-
-        		if ( this.check_if_bomb( index_coords ) ) {
+        		if ( this.check_if_bomb( index ) ) {
         			this.game_over = true;
         			this.show_all_bombs();
         		} else {
-        			this.reveal_tile( index_coords, index );
-
+        			this.reveal_tile( index );
         		}
         	},
 
         	show_all_bombs () {
         		this.bombs_coordinates.forEach( function ( bomb_coord ) {
-        			const bomb_split_coords = bomb_coord.split(',');
-
-        			document.querySelector(`#ms-tile-${( bomb_split_coords[0] * 12 ) + ( bomb_split_coords[1] % 12 )}`).classList.add('bomb');	
+        			document.querySelector(`#ms-tile-${ bomb_coord }`).classList.add('bomb');	
         		} );
         	},
 
-        	reveal_tile ( coords, index ) {
-        		const surrounding_bomb_count = this.check_for_surrounding_bombs( coords );
-        		document.querySelector(`#ms-tile-${index}`).classList.add(`touching-${surrounding_bomb_count}`, 'clicked');
+        	reveal_tile ( index ) {
+        		const surrounding_bomb_count = this.check_for_surrounding_bombs( index );
+        		document.querySelector(`#ms-tile-${ index }`).classList.add(`touching-${ surrounding_bomb_count }`, 'clicked');
         	},
 
-        	check_for_surrounding_bombs ( coords ) {
+        	check_for_surrounding_bombs ( index ) {
         		let surrounding_bomb_count = 0;
-
-        		for ( let i = 0; i < 8; i ++ ) {
-        			let y_coords_offset = i < 3 ? -1 : i < 5 ? 0 : 1,
-        				x_coords_offset = ( ( i + ( i > 3 ? 1 : 0 ) ) % 3 ) - 1,
-        				to_check_coords = ( coords.x + x_coords_offset ).toString() + ',' + ( coords.y + y_coords_offset ).toString();
-
-        			if ( this.bombs_coordinates.indexOf( to_check_coords ) !== -1 ) surrounding_bomb_count++;
-        		}
+                
+                for ( let i = 0; i < 8; i ++ ) {
+                    let y_index_offset = ( i < 3 ? -1 : i < 5 ? 0 : 1 ) * this.minesweeper_config.width,
+                        x_index_offset = ( ( i + ( i > 3 ? 1 : 0 ) ) % 3 ) - 1,
+                        to_check_index = index + y_index_offset + x_index_offset;
+                    
+                    if ( this.bombs_coordinates.indexOf( to_check_index ) !== -1 ) surrounding_bomb_count++;
+                }
 
         		return surrounding_bomb_count;
         	},
 
-        	check_if_bomb ( coords ) {
-        		return this.bombs_coordinates.indexOf( coords.x + ',' + coords.y ) === -1 ? false : true;
+        	check_if_bomb ( bomb_index ) {
+        		return this.bombs_coordinates.indexOf( bomb_index ) === -1 ? false : true;
         	},
 
         	populate_grid_with_bombs () {
         		let i = 0;
 
         		while( i < this.minesweeper_config.bomb_count ) {
-        			const 	new_bomb_coords = ( Math.floor( Math.random( this.minesweeper_config.width ) * this.minesweeper_config.width ) + 1 ).toString() + ',' + ( Math.floor( Math.random( this.minesweeper_config.height ) * this.minesweeper_config.height ) + 1 ).toString();
+        			const 	new_bomb_index = Math.ceil( Math.random() * ( this.minesweeper_config.width * this.minesweeper_config.height ) );
 
-        			if ( this.bombs_coordinates.indexOf( new_bomb_coords ) === -1 ) {
-        				this.bombs_coordinates.push( new_bomb_coords );
+        			if ( this.bombs_coordinates.indexOf( new_bomb_index ) === -1 ) {
+        				this.bombs_coordinates.push( new_bomb_index );
         				i++;
         			}
         		}
@@ -158,9 +155,25 @@
 			border-right: 2px solid darken( #C3C6CB, 20% );
             border-bottom: 2px solid darken( #C3C6CB, 10% );
             background-size: 100% 100%;
+            position: relative;
 
             &.bomb {
             	background-image: url('../assets/images/minesweeper/bomb.png')
+            }
+            
+            @for $i from 0 through 8 {
+                &.touching-#{$i} {
+                    &:after {
+                        content: "#{$i}";
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        width: 100%;
+                        height: 100%;
+                        font-size: 1rem;
+                        color: #000;
+                    }
+                }
             }
 		}
 	}
